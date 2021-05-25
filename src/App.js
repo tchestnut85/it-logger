2fba379676c43d92ebb969f208f4a75693a86395
Tom Chestnut <tchestnut85@gmail.com>
2021-05-24 20:26:32 -0400

    Installed Materialize-CSS and initialized the JS modules in App.js.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useEffect } from 'react';
+
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const App = () => {
+	// Initialize Materialize JavaScript
+	useEffect(() => {
+		M.AuthInit();
+	});
+	return <div className='App'>It Logger</div>;
+};
 
 export default App;

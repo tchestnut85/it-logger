b768a74c70f8bde906d306b120acb6e63e7b81c6
Tom Chestnut <tchestnut85@gmail.com>
2021-05-26 19:09:56 -0400

    Created a Logs component.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import 'materialize-css/dist/css/materialize.min.css';
 
 import React, { useEffect } from 'react';
 
+import Logs from './components/layout/logs/Logs';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
 
 const App = () => {
 	// Initialize Materialize JavaScript
 	useEffect(() => {
 		M.AuthInit();
 	});
-	return <div className='App'>It Logger</div>;
+
+	return (
+		<>
+			<SearchBar />
+			<div className='container'>
+				<Logs />
+			</div>
+		</>
+	);
 };
 
 export default App;

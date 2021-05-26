57ec538d0c21118c71dbf4704881202bc60a5838
Tom Chestnut <tchestnut85@gmail.com>
2021-05-26 19:51:31 -0400

    Added preloader component to display while data is loading.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 
 import React, { useEffect } from 'react';
 
-import Logs from './components/layout/logs/Logs';
+import Logs from './components/logs/Logs';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
 

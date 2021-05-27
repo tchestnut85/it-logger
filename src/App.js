eb472f011e57acfa86b618781f1c61f85cb6e743
Tom Chestnut <tchestnut85@gmail.com>
2021-05-26 20:17:09 -0400

    Added a log modal to add a log item.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 
 import React, { useEffect } from 'react';
 
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import Logs from './components/logs/Logs';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
@@ -17,6 +19,8 @@ const App = () => {
 		<>
 			<SearchBar />
 			<div className='container'>
+				<AddBtn />
+				<AddLogModal />
 				<Logs />
 			</div>
 		</>

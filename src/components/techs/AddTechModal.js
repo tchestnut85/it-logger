a15294a43b1a55baab76d0eb90a42c823c10a2e3
Tom Chestnut <tchestnut85@gmail.com>
2021-06-01 20:36:40 -0400

    Added Redux action and reducer for adding a new tech.

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
+import PropTypes from 'prop-types';
+import { addTech } from '../../utils/actions/techActions';
+import { connect } from 'react-redux';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
 	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState(false);
+	const [lastName, setLastName] = useState('');
 
 	const onSubmit = () => {
 		if (firstName === '' || lastName === '') {
 			M.toast({ html: 'Please enter first and last name' });
 		} else {
-			console.log(firstName, lastName);
+			addTech({
+				firstName,
+				lastName,
+			});
+
+			M.toast({
+				html: `${firstName} ${lastName} was succesfully added.`,
+			});
 
 			// Clear fields
 			setFirstName('');
@@ -68,4 +78,8 @@ const modalStyle = {
 	height: '75%',
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+	addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
